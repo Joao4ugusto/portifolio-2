2dd54b998fdb32f50cdd14df1e49a08371953685
João <joaoaugusto311016@gmail.com>
2023-05-31 12:31:34 -0300

    31-05

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -1,10 +1,10 @@
 export default function Principal(){
   return (
     <div className="mx-3 my-28 md:mx-28 md:my-14 lg:mx-52 xl:mx-64">
-     <h1 className="text-white text-base font-medium">Olá, eu sou o <br/>
-         <span className=" font-semibold text-5xl">João Augusto<i className=" text-rose-950">..</i></span></h1>
-      <h3 className=" mt-2 text-lg font-normal">Desenvolvedor de Software | Estudante <br/> de Ciências da Computação.</h3>
-      <div className=" mt-5 w-full bg-color-black-sidebar border p-3">
+     <h1 className=" text-gray-300 text-base font-medium">OLÁ, EU SOU O <br/>
+         <span className=" text-white font-semibold text-5xl ">João <span className="">Augusto..</span></span></h1>
+      <h3 className=" mt-2 text-lg font-light">Desenvolvedor de Software | Estudante <br/> de Ciências da Computação.</h3>
+      <div className=" mt-5 w-full bg-color-black-sidebar border border-stone-900 p-3">
       <h4 className=" underline">Quem eu sou?</h4>
       <p className=" font-light mt-2 leading-8">Sou um Desenvolvedor especializado em HTML, CSS, JavaScript e TypeScript, com conhecimento em ReactJS, NextJS, Tailwind CSS. Também possuo habilidades avançadas em Java e Spring para o desenvolvimento do backend.
         Com paixão pela inovação, crio experiências cativantes e humanas, buscando sempre aprender novas tecnologias para fornecer soluções criativas e eficientes. Meu objetivo principal é proporcionar uma experiência excepcional ao usuário, em qualquer dispositivo.</p>
