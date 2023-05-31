a6c9b44dd3b7406daa21f1f60f121ed8bd692e6a
João <joaoaugusto311016@gmail.com>
2023-05-31 15:15:15 -0300

    31-05

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import IconBxlLinkedinSquare from "./icons/iconLinkedin";
 
 export default function Footer() {
   return (
-    <footer className=" bottom-0 relative px-14 py-6 w-full h-[80px] md:bg-color-black-sidebar border-t-2 border-stone-900">
+    <footer className=" bottom-0 fixed px-14 py-6 w-full h-[80px] md:bg-color-black-sidebar border-t-2 border-stone-900">
       <div className="flex items-center justify-between">
         <div>
           <a href="#"><IconBxlInstagram width={22} height={22} /></a>
