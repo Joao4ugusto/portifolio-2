a7ccbf5572408dfc4f71c2fa4e81382c3dba33e4
João <joaoaugusto311016@gmail.com>
2023-05-31 13:16:35 -0300

    31-05

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -2,7 +2,7 @@ export default function Principal(){
   return (
     <div className="mx-3 my-32 md:mx-28 md:my-14 lg:mx-52 xl:mx-64">
      <h1 className=" text-gray-300 text-base font-medium">OLÁ, EU SOU O <br/>
-         <span className=" text-white font-semibold text-5xl ">João <span className=" font-semibold bg-gradient-to-br from-pink-600 to-orange-500 text-transparent bg-clip-text">Augusto..</span></span></h1>
+         <span className=" text-white font-semibold text-5xl ">João <span className=" font-bold bg-gradient-to-br from-pink-600 to-orange-500 text-transparent bg-clip-text">Augusto..</span></span></h1>
       <h3 className=" mt-2 text-lg font-light">Desenvolvedor de Software | Estudante <br/> de Ciências da Computação.</h3>
       <div className=" mt-5 w-full bg-color-black-sidebar border border-stone-800 p-3">
       <h4 className=" underline">Quem eu sou?</h4>
