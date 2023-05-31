2788800b1aee873294b397d59205b6c27b397e90
João <joaoaugusto311016@gmail.com>
2023-05-31 11:10:50 -0300

    31-05

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -4,8 +4,11 @@ export default function Principal(){
      <h1 className="text-white text-base font-medium">Olá, eu sou o <br/>
          <span className=" font-semibold text-5xl">João Augusto<i className=" text-rose-950">..</i></span></h1>
       <h3 className=" mt-2 text-lg font-normal">Desenvolvedor de Software | Estudante <br/> de Ciências da Computação.</h3>
+      <div className=" mt-5 w-full bg-color-black-sidebar border p-3">
+      <h4 className=" underline">Quem eu sou?</h4>
       <p className=" font-light mt-2 leading-8">Sou um Desenvolvedor especializado em HTML, CSS, JavaScript e TypeScript, com conhecimento em ReactJS, NextJS, Tailwind CSS. Também possuo habilidades avançadas em Java e Spring para o desenvolvimento do backend.
         Com paixão pela inovação, crio experiências cativantes e humanas, buscando sempre aprender novas tecnologias para fornecer soluções criativas e eficientes. Meu objetivo principal é proporcionar uma experiência excepcional ao usuário, em qualquer dispositivo.</p>
+      </div>
     </div>
   )
 }
