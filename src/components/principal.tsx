919590454bb92b9e476ccd678fcd232c28228164
João <joaoaugusto311016@gmail.com>
2023-06-02 12:03:21 -0300

    02-06

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -13,8 +13,8 @@ import IconBxlTailwindCss from "./icons/taiwind";
 export default function Principal(){
   return (
     <div className="mx-3 my-24 md:mx-10 md:my-14 lg:mx-96 xl:mx-96 md:absolute">
-     <h1 className=" text-slate-200 text-lg font-medium">OLÁ, EU SOU O <br/>
-         <span className=" text-white font-semibold text-5xl ">João <span className=" font-bold bg-gradient-to-br from-pink-600 to-orange-500 text-transparent bg-clip-text">Augusto..</span></span></h1>
+     <h1 className=" italic text-slate-200 text-lg font-medium">OLÁ, EU SOU O <br/>
+         <span className=" italic text-white font-semibold text-5xl ">João <span className=" font-bold bg-gradient-to-br from-pink-600 to-orange-500 text-transparent bg-clip-text">Augusto..</span></span></h1>
       <h3 className=" mt-2 text-lg font-light">Desenvolvedor de Software | Estudante <br/> de Ciências da Computação.</h3>
       <div className=" hover:bg-color-black-sidebar/50 mt-5 w-full bg-color-black-sidebar border border-stone-800 rounded p-4">
       <h4 className=" underline text-base">Quem eu sou?</h4>
@@ -32,7 +32,7 @@ export default function Principal(){
   </div>
        </div>
       </div>
-      <h3 className=" mt-10 text-lg underline">Tecnologias</h3>
+      <h3 className=" mt-10 text-lg underline italic">Tecnologias</h3>
       <div className="flex mt-8 gap-4 w-full">
         <div className=" hover:text-sky-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconReact  width={24} height={24} /></div>
         <div className=" hover:text-amber-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconJavascript width={24} height={24} /></div>
