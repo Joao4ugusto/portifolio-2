72ae01dba271af7f47d1078ffdbce0a198943f70
João <joaoaugusto311016@gmail.com>
2023-06-03 00:25:21 -0300

    03-06

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -12,8 +12,8 @@ import IconBxlTailwindCss from "./icons/taiwind";
 
 export default function Principal(){
   return (
-    <div className="mx-3 my-24 md:mx-10 md:my-14 lg:mx-96 xl:mx-96 md:absolute">
-     <h1 className=" italic text-slate-200 text-lg font-medium">OLÁ, EU SOU O <br/>
+    <div className="mx-3 my-28 md:mx-10 md:my-14 lg:mx-96 xl:mx-96 md:absolute">
+     <h1 className=" italic text-slate-200 text-lg font-medium">PRAZER, EU SOU <br/>
          <span className=" italic text-white font-semibold text-5xl ">João <span className=" font-bold bg-gradient-to-br from-pink-600 to-orange-500 text-transparent bg-clip-text">Augusto..</span></span></h1>
       <h3 className=" mt-2 text-lg font-light">Desenvolvedor de Software | Estudante <br/> de Ciências da Computação.</h3>
       <div className=" hover:bg-color-black-sidebar/50 mt-5 w-full bg-color-black-sidebar border border-stone-800 rounded p-4">
@@ -33,13 +33,13 @@ export default function Principal(){
        </div>
       </div>
       <h3 className=" mt-10 text-xs underline italic">TECNOLOGIAS</h3>
-      <div className="flex mt-8 gap-4 w-full">
+      <div className="flex mt-8 gap-5 w-full">
         <div className=" transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconReact  width={24} height={24} /></div>
         <div className=" transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconJavascript width={24} height={24} /></div>
         <div className=" transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconLanguageHtml5 width={24} height={24} /></div>
         <div className=" transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconBxlCss3 width={24} height={24}  /></div>
       </div>
-      <div className="flex mt-5 mb-8 gap-4 w-full">
+      <div className="flex mt-5 mb-8 gap-5 w-full">
       <div className="   transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconBxlSass  width={24} height={24} /></div>
         <div className=" transition-transform duration-500 transform hover:-translate-y-3 p-3  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconJava  width={35} height={35} /></div>
         <div className=" transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconBxlSpringBoot width={24} height={24} /></div>
