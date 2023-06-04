fb18a5556398d5fc11920b59ccd6d0db9d579960
João <joaoaugusto311016@gmail.com>
2023-06-04 00:55:20 -0300

    04-06

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -13,7 +13,7 @@ import IconBxlTailwindCss from "./icons/taiwind";
 export default function Principal(){
   return (
     <div className=" z-30 mx-4 my-28 md:mx-10 md:my-14 lg:mx-96 xl:mx-96 md:absolute">
-      <div className="absolute right-6 text-cyan-500 rotating-icon animate-spin duration-400"><IconReact  width={28} height={28}/></div>
+      <div className="absolute right-6 text-cyan-500 rotating-icon animate-spin duration-400"><IconReact  width={32} height={32}/></div>
      <h1 className=" italic text-slate-200 text-lg font-medium">OLÁ, EU SOU O <br/>
          <span className=" italic text-white font-semibold text-5xl ">João <span className=" font-bold bg-gradient-to-br from-pink-600 to-orange-500 text-transparent bg-clip-text">Augusto..</span></span></h1>
       <h3 className=" mt-2 text-lg font-light">Desenvolvedor de Software | Estudante <br/> de Ciências da Computação.</h3>
