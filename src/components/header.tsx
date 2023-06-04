9f4d45d58081bbc253d4422aa0c6af941fe0d863
João <joaoaugusto311016@gmail.com>
2023-06-04 15:05:15 -0300

    04-06

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export default function Header({ openSaidebarClick }: props){
     <header className=" z-40 md:hidden fixed bg-black w-full">
       <div className=" h-[56px] my-3 mx-4 flex justify-between items-center">
       <div>
-        <h1 className="underline text-base font-normal">João <br/> Augusto</h1>
+        <h1 className="underline text-base font-medium">João <br/> Augusto</h1>
       </div>
       <div onClick={openSaidebarClick}>
         <IconMenu width={24} height={24} />
