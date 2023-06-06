572ee256cca726bc9dd5d31a05f304405af4e11f
João <joaoaugusto311016@gmail.com>
2023-06-06 15:08:56 -0300

    06-06

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -22,17 +22,17 @@ export default function Principal(){
         Com paixão pela inovação, crio experiências cativantes e humanas, buscando sempre aprender novas tecnologias para fornecer soluções criativas e eficientes. Meu objetivo principal é proporcionar uma experiência excepcional ao usuário, em qualquer dispositivo.</p>
       </div>
       <div className=" flex my-5 gap-5">
-      <div className=" p-2 bg-color-black-sidebar border rounded border-stone-800 text-center">WhatsApp</div>
+      <div className=" p-2 bg-color-black-sidebar border rounded border-stone-800 text-center">Contato</div>
       <div className="flex p-2 bg-color-black-sidebar border rounded border-stone-800 text-center">
       <div className="flex items-center mr-2">
       <IconEmail width={18} />
       </div>
-  <div className="flex items-center">
-    E-mail
-  </div>
+      <div className="flex items-center">
+      E-mail
+      </div>
        </div>
       </div>
-      <div className="flex mt-12 gap-4">
+      <div className="flex flex-wrap mt-12 gap-6 justify-center md:justify-normal">
         <div className=" hover:text-cyan-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconReact  width={24} height={24} /></div>
         <div className=" hover:text-yellow-400 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconJavascript width={24} height={24} /></div>
         <div className=" hover:text-orange-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconLanguageHtml5 width={24} height={24} /></div>
