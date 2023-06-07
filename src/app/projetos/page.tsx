4f3c2d541ae7e96160d6656bfb2c2ab1e1b4e263
João <joaoaugusto311016@gmail.com>
2023-06-07 17:58:24 -0300

    07-06

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -5,6 +5,8 @@ import IconArrowUpRightSquareFill from '@/components/icons/arrow';
 
 export default function Principal(){
   const [sidebarOpened, setSidebarOpened] =  useState(false);
+  
+  const nome = 'React';
 
   const openSaidebar = () => setSidebarOpened(true)
   const clouseSaidebar = () => setSidebarOpened(false)
@@ -17,10 +19,10 @@ export default function Principal(){
         <div className=' mt-10 xl:w-[550px] xl:h-[240px] hover:bg-color-black-sidebar/50 w-full bg-color-black-sidebar border border-stone-800 rounded'>
           <h1 className=' justify-center items-center text-center'>ooi</h1>
           <div className='border-t mt-36 border-stone-800 p-3'>
-            <div>
-             <h2 className=' text-center w-16 bg-black border rounded p-2 border-stone-800 cursor-pointer'>Titulo</h2>
-             <div>
-              <IconArrowUpRightSquareFill width={26} height={26} />
+            <div className=' flex justify-between items-center'>
+             <h2 className=' text-center w-16 bg-black border rounded p-2 border-stone-800 cursor-pointer'>{nome}</h2>
+             <div className=' cursor-pointer'>
+              <IconArrowUpRightSquareFill width={28} height={28} />
              </div>
             </div>
          </div>
