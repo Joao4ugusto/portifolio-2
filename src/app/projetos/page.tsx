d1a5b69eb802557bc556a6d7509e212ad36ca58f
João <joaoaugusto311016@gmail.com>
2023-06-07 17:21:33 -0300

    07-06

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -13,7 +13,7 @@ export default function Principal(){
        </Sidebar>
        <div className='mx-4 my-28 md:mx-18 md:ml-[280px] md:my-14 lg:mx-96 xl:ml-[550px] xl:mx-[310px] md:absolute'>
         <h1 className=' text-white text-2xl font-semibold'>Projetos</h1>
-        <div className=' mt-10 xl:w-[500px] xl:h-[250px] hover:bg-color-black-sidebar/50 w-full bg-color-black-sidebar border border-stone-800 rounded'>
+        <div className=' mt-10 xl:w-[550px] xl:h-[240px] hover:bg-color-black-sidebar/50 w-full bg-color-black-sidebar border border-stone-800 rounded'>
          <div className='border-t mt-36 border-stone-800 p-3'>
          <div>
           <h2>Titulo</h2>
