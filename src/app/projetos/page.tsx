1be72717c6f074a03101c1b3faae6cb84df02677
João <joaoaugusto311016@gmail.com>
2023-06-07 17:37:02 -0300

    07-06

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import {useState} from 'react'
 import Sidebar from "@/components/sidebar";
+import IconArrowUpRightSquareFill from '@/components/icons/arrow';
 
 export default function Principal(){
   const [sidebarOpened, setSidebarOpened] =  useState(false);
@@ -14,11 +15,14 @@ export default function Principal(){
        <div className='mx-4 my-28 md:mx-18 md:ml-[280px] md:my-14 lg:mx-96 xl:ml-[550px] xl:mx-[310px] md:absolute'>
         <h1 className=' text-white text-2xl font-semibold'>Projetos</h1>
         <div className=' mt-10 xl:w-[550px] xl:h-[240px] hover:bg-color-black-sidebar/50 w-full bg-color-black-sidebar border border-stone-800 rounded'>
-         <div className='border-t mt-36 border-stone-800 p-3'>
-         <div>
-          <h2>Titulo</h2>
-          
-         </div>
+          <h1 className=' justify-center items-center text-center'>ooi</h1>
+          <div className='border-t mt-36 border-stone-800 p-3'>
+            <div>
+             <h2 className=' text-center w-16 bg-black border rounded p-2 border-stone-800 cursor-pointer'>Titulo</h2>
+             <div>
+              <IconArrowUpRightSquareFill width={26} height={26} />
+             </div>
+            </div>
          </div>
         </div>
        </div>
