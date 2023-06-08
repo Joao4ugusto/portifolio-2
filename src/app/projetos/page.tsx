121990acca5e07b3a5823fc10d04ce258372ee6c
João <joaoaugusto311016@gmail.com>
2023-06-08 17:48:42 -0300

    08-06

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,9 +1,11 @@
 "use client"
+import Image from 'next/image';
 import {useState} from 'react'
 import Sidebar from "@/components/sidebar";
 import IconArrowUpRightSquareFill from '@/components/icons/arrow';
 import IconJavascript from '@/components/icons/javaScript';
-import IconReact from '@/components/icons/react';
+import Gradiente from '@/components/icons/gradiente';
+import Header from '@/components/header';
 
 export default function Principal(){
   const [sidebarOpened, setSidebarOpened] =  useState(false);
@@ -14,33 +16,32 @@ export default function Principal(){
   const clouseSaidebar = () => setSidebarOpened(false)
   return(
     <div>
+      <Header />
       <Sidebar open={sidebarOpened} onClose={clouseSaidebar}>
        </Sidebar>
-      <div className="mx-4 my-28 md:mx-18 md:ml-[280px] md:my-14 lg:mx-96 xl:ml-[570px] xl:mx-[310px] md:absolute">
-        <h1 className="text-white text-2xl font-semibold">Projetos</h1>
-        <div className="mt-10 xl:w-[550px] xl:h-[240px] hover:bg-color-black-sidebar/50 w-full bg-color-black-sidebar border border-stone-800 rounded">
-          <div className=" mt-18 mb-20 text-center justify-center justify-items-center">
-            <h1>ooooi</h1>
+      <div className=" mx-4 my-28 md:mx-18 md:ml-[280px] md:my-14 lg:mx-96 xl:ml-[570px] xl:mx-[310px] md:absolute">
+          <h1 className="text-white text-2xl font-semibold">Projetos</h1>
+
+          <div className=' mt-10 bg-color-black-sidebar w-[560px] h-60 border rounded border-stone-800'>
+          <div className=' flex h-[70%] justify-center items-center center'>
+            <Gradiente />
           </div>
-          <div className=" h-3 border-t border-stone-800 p-4">
-            <div className="flex justify-between items-center">
-              <div className="flex gap-3">
-                <h2 className="text-center bg-black border rounded p-2 border-stone-800 cursor-pointer">{nome}</h2>
-                <div className="hover:text-yellow-400 bg-black border rounded p-2 border-stone-800 cursor-pointer">
-                  <IconJavascript width={24} height={24} />
-                </div>
-                <div className="hover:text-cyan-500 bg-black border rounded p-2 border-stone-800 cursor-pointer">
-                  <IconReact width={24} height={24} />
-                </div>
+          <div className=' justify-between flex border-t border-stone-800 p-3'>
+            <div className=' flex gap-4'>
+              <div className='p-2 bg-black border rounded border-stone-800 inline-block'>
+                <h1>{nome}</h1>
               </div>
-              <div className="cursor-pointer">
-                <IconArrowUpRightSquareFill width={28} height={28} />
+              <div className=' p-2 bg-black border rounded border-stone-800 inline-block'>
+                <IconJavascript width={28} height={28} />
               </div>
             </div>
+            <div className=' flex items-center'>
+              <IconArrowUpRightSquareFill width={30} height={30} />
+            </div>
           </div>
-        </div>
-</div>
+          </div>
 
+        </div>
     </div>
   )
 }
