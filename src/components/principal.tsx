e017f3d083ef289be9761bf1ad71d69edbfb4e00
João <joaoaugusto311016@gmail.com>
2023-06-09 23:00:51 -0300

    09-06

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -17,8 +17,8 @@ export default function Principal(){
       <h3 className=" mt-3 text-lg font-light">Por meio de prática e aprendizado constantes, produzo software estético com um padrão extremamente alto.</h3>
 
       <div className=" flex my-5 gap-5">
-      <div className=" cursor-pointer p-3 bg-color-black-sidebar border rounded border-stone-800 text-center"><a href="">Contato</a></div>
-      <div className="flex p-3 border rounded border-stone-800 text-center">
+      <div className=" cursor-pointer p-3 bg-color-black-sidebar border rounded border-stone-700 text-center"><a href="">Contato</a></div>
+      <div className="flex p-3 border rounded border-stone-700 text-center">
       <div className="flex items-center mr-2">
       <IconEmail width={18} />
       </div>
@@ -28,22 +28,22 @@ export default function Principal(){
        </div>
       </div>
 
-      <div className=" hover:bg-color-black-sidebar/50 mt-5 w-full bg-color-black-sidebar border border-stone-800 rounded p-4">
+      <div className=" hover:bg-color-black-sidebar/50 mt-5 w-full bg-color-black-sidebar border border-stone-700 rounded p-4">
       <h4 className="text-base">Quem eu sou?</h4>
       <p className=" font-light mt-5 leading-8 ">Sou um Desenvolvedor especializado em HTML, CSS, JavaScript e TypeScript, com conhecimento em ReactJS, NextJS, Tailwind CSS. Também possuo habilidades avançadas em Java e Spring para o desenvolvimento do backend.
         Com paixão pela inovação, crio experiências cativantes e humanas, buscando sempre aprender novas tecnologias para fornecer soluções criativas e eficientes. Meu objetivo principal é proporcionar uma experiência excepcional ao usuário, em qualquer dispositivo.</p>
       </div>
       
       <div className="flex flex-wrap mt-12 gap-y-4 gap-x-1 justify-between">
-        <div className=" hover:text-cyan-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconReact  width={24} height={24} /></div>
-        <div className=" hover:text-yellow-400 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconJavascript width={24} height={24} /></div>
-        <div className=" hover:text-orange-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconLanguageHtml5 width={24} height={24} /></div>
-        <div className=" hover:text-sky-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconBxlCss3 width={24} height={24}  /></div>
-        <div className=" hover:text-pink-400 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconBxlSass  width={24} height={24} /></div>
-        <div className=" hover:text-teal-600 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconBxlTailwindCss  width={24} height={24} /></div>
-        <div className=" hover:text-green-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconBxlSpringBoot width={24} height={24} /></div>
-        <div className=" hover:text-cyan-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconBxlPostgresql width={24} height={24}  /></div>
-        <div className=" hover:text-green-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-800 rounded"><IconBxlMongodb width={24} height={24} /></div>
+        <div className=" hover:text-cyan-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-700 rounded"><IconReact  width={24} height={24} /></div>
+        <div className=" hover:text-yellow-400 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-700 rounded"><IconJavascript width={24} height={24} /></div>
+        <div className=" hover:text-orange-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-700 rounded"><IconLanguageHtml5 width={24} height={24} /></div>
+        <div className=" hover:text-sky-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-700 rounded"><IconBxlCss3 width={24} height={24}  /></div>
+        <div className=" hover:text-pink-400 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-700 rounded"><IconBxlSass  width={24} height={24} /></div>
+        <div className=" hover:text-teal-600 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-700 rounded"><IconBxlTailwindCss  width={24} height={24} /></div>
+        <div className=" hover:text-green-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-700 rounded"><IconBxlSpringBoot width={24} height={24} /></div>
+        <div className=" hover:text-cyan-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-700 rounded"><IconBxlPostgresql width={24} height={24}  /></div>
+        <div className=" hover:text-green-500 transition-transform duration-500 transform hover:-translate-y-3 p-5  w-16 h-16 bg-color-black-sidebar border border-stone-700 rounded"><IconBxlMongodb width={24} height={24} /></div>
       <div>
        </div>
       </div>
