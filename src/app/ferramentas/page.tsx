f2e25d5e64543e0e93936a707a637d2655a318bf
João <joaoaugusto311016@gmail.com>
2023-06-11 00:08:00 -0300

    11-06

--- src/app/ferramentas/page.tsx
+++ src/app/ferramentas/page.tsx
@@ -1,5 +1,17 @@
+"use client"
+import Sidebar from "@/components/sidebar";
+import { useState } from "react";
+
 export default function Ferramentas(){
+  const [sidebarOpened, setSidebarOpened] =  useState(false);
+
+  const openSaidebar = () => setSidebarOpened(true)
+  const clouseSaidebar = () => setSidebarOpened(false)
+  
   return(
-    <div>oopaaa</div>
+    <div>
+      <Sidebar open={sidebarOpened} onClose={clouseSaidebar}>
+       </Sidebar>
+    </div>
   )
 }
