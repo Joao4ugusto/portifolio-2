5ca0b1027dc8cf512f1f24d739683e11eed0e5bb
João <joaoaugusto311016@gmail.com>
2023-06-11 14:49:29 -0300

    11-06

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -8,20 +8,20 @@ import IconReact from "./icons/react";
 import IconBxlSass from "./icons/scss";
 import IconBxlSpringBoot from "./icons/spring";
 import IconBxlTailwindCss from "./icons/taiwind";
-import minhaImagem from "../../public/download (2) 1.png"
+import minhaImagem from "../../public/Fundo de tela 1.png"
 import Image from "next/image";
 
 export default function Principal(){
   return (
-    <div className=" z-30 mx-5 my-28 md:mx-18 md:ml-[280px] md:my-14 lg:mx-96 xl:ml-[580px] xl:mx-[315px] 2xl:ml-[620px]  2xl:mx-[350px] md:absolute">
+    <div className=" z-30 mx-5 my-28 md:mx-18 md:ml-[280px] md:my-14 lg:mx-96 xl:ml-[580px] xl:mx-[390px] 2xl:ml-[620px]  2xl:mx-[350px] md:absolute">
       <div className=" flex items-center gap-3">
          <h1 className=" text-[46px] text-5xl font-semibold">João <span className=" text-roxo">  Augusto </span></h1>
          <div className="">
           <IconReact style={{ animationDuration: 's', animationTimingFunction: 'linear', animationIterationCount: 'infinite' }} width={32} height={32} className="text-cyan-500 flex animate-spin" />
          </div>
       </div>
-      <h3 className=" text-white mt-3 text-lg font-medium">Desenvolvedor de software | Estudante de <br/> Ciências da Computação</h3>
-      <h3 className=" lg:w-[380px] flex-wrap text-gray-300 mt-2 text-base font-light">Por meio de prática e aprendizado constantes, produzo software estético com um padrão extremamente alto.</h3>
+      <h3 className=" text-white mt-3 text-base font-medium">Desenvolvedor de software | Estudante de <br/> Ciências da Computação</h3>
+      <h3 className=" lg:w-[380px] flex-wrap text-gray-300 mt-2 text-sm font-light">Por meio de prática e aprendizado constantes, produzo software estético com um padrão extremamente alto.</h3>
 
       <div className=" flex my-5 gap-5 cursor-pointer">
       <div className=" py-2 px-3 bg-color-black-sidebar border rounded border-stone-800 text-center"><a href="">Projetos</a></div>
@@ -36,7 +36,7 @@ export default function Principal(){
       </div>
 
       <div className=" hover:bg-color-black-sidebar/50 mt-8 w-full bg-color-black-sidebar  border border-stone-800 rounded p-4">
-      <Image width={400} src={minhaImagem} alt="imagem"></Image>
+      <Image src={minhaImagem} alt="imagem" className=" w-full"/>
       <h4 className=" mt-2 text-lg text-roxo">Quem eu sou?</h4>
       <p className=" font-light mt-5 leading-10 ">Sou um Desenvolvedor especializado em HTML, CSS, JavaScript e TypeScript, com conhecimento em ReactJS, NextJS, Tailwind CSS. Também possuo habilidades avançadas em Java e Spring para o desenvolvimento do backend.
         Com paixão pela inovação, crio experiências cativantes e humanas, buscando sempre aprender novas tecnologias para fornecer soluções criativas e eficientes. Meu objetivo principal é proporcionar uma experiência excepcional ao usuário, em qualquer dispositivo.</p>
