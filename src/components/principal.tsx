4b3053bba69cb2e8809cfacd19947a8391217e27
João <joaoaugusto311016@gmail.com>
2023-06-11 11:58:22 -0300

    11-06

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -13,13 +13,13 @@ export default function Principal(){
   return (
     <div className=" z-30 mx-5 my-28 md:mx-18 md:ml-[280px] md:my-14 lg:mx-96 xl:ml-[580px] xl:mx-[315px] 2xl:ml-[620px]  2xl:mx-[350px] md:absolute">
       <div className=" flex items-center gap-3">
-         <h1 className=" text-5xl font-semibold">João <span className=" text-roxo">  Augusto </span></h1>
+         <h1 className=" text-[46px] text-5xl font-semibold">João <span className=" text-roxo">  Augusto </span></h1>
          <div className="">
           <IconReact style={{ animationDuration: 's', animationTimingFunction: 'linear', animationIterationCount: 'infinite' }} width={32} height={32} className="text-cyan-500 flex animate-spin" />
          </div>
       </div>
       <h3 className=" text-white mt-3 text-lg font-light">Desenvolvedor de software | Estudante de <br/> Ciências da Computação</h3>
-      <h3 className=" flex-wrap text-gray-300 mt-2 text-lg font-light">Por meio de prática e aprendizado constantes, produzo software estético com um padrão extremamente alto.</h3>
+      <h3 className=" flex-wrap text-gray-300 mt-2 text-base font-light">Por meio de prática e aprendizado constantes, produzo software estético com um padrão extremamente alto.</h3>
 
       <div className=" flex my-5 gap-5 cursor-pointer">
       <div className=" py-2 px-3 bg-color-black-sidebar border rounded border-stone-800 text-center"><a href="">Projetos</a></div>
