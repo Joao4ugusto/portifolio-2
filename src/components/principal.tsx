58bd9ee0181fc4e0b7fb2dfb725b8695a6ca4539
João <joaoaugusto311016@gmail.com>
2023-06-12 15:14:35 -0300

    12-06

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -32,7 +32,7 @@ export default function Principal(){
       </div>
 
       <div className=" lg:mb-16 xl:mb-16 hover:bg-color-black-sidebar/50 mt-8 w-full bg-color-black-sidebar border border-stone-800 rounded p-4">
-      <h4 className=" text-lg text-roxo font-semibold">Quem eu sou?</h4>
+      <h4 className=" text-lg text-roxo font-bold">Quem eu sou?</h4>
       <p className=" font-light mt-5 leading-8 ">Sou um Desenvolvedor especializado em HTML, CSS, JavaScript e TypeScript, com conhecimento em ReactJS, NextJS, Tailwind CSS. Também possuo habilidades avançadas em Java e Spring para o desenvolvimento do backend.
         Com paixão pela inovação, crio experiências cativantes e humanas, buscando sempre aprender novas tecnologias para fornecer soluções criativas e eficientes. Meu objetivo principal é proporcionar uma experiência excepcional ao usuário, em qualquer dispositivo.</p>
 
