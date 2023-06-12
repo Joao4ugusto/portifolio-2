b21e7049fba6d32f6fabc112427aad44d3cb6066
João <joaoaugusto311016@gmail.com>
2023-06-12 14:54:37 -0300

    12-06

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -9,7 +9,7 @@ import IconReact from "./icons/react";
 
 export default function Principal(){
   return (
-    <div className=" z-30 mx-5 my-28 md:my-20 md:mx-[55px] md:ml-[330px]  lg:mx-[100px] lg:ml-[400px] xl:ml-[520px] xl:mx-[210px] 2xl:ml-[565px]  2xl:mx-[300px] md:absolute">
+    <div className=" z-30 mx-5 my-28 md:my-28 lg:my-14 md:mx-[55px] md:ml-[330px]  lg:mx-[100px] lg:ml-[400px] xl:ml-[520px] xl:mx-[240px] 2xl:ml-[565px]  2xl:mx-[300px] md:absolute">
       <div className=" flex items-center gap-3">
          <h1 className=" text-[46px] text-5xl xl:text-[55px] font-semibold">João <span className=" text-roxo">  Augusto </span></h1>
          <div className="">
@@ -32,7 +32,7 @@ export default function Principal(){
       </div>
 
       <div className=" lg:mb-16 xl:mb-16 hover:bg-color-black-sidebar/50 mt-8 w-full bg-color-black-sidebar border border-stone-800 rounded p-4">
-      <h4 className=" text-lg text-roxo">Quem eu sou?</h4>
+      <h4 className=" text-lg text-roxo font-semibold">Quem eu sou?</h4>
       <p className=" font-light mt-5 leading-8 ">Sou um Desenvolvedor especializado em HTML, CSS, JavaScript e TypeScript, com conhecimento em ReactJS, NextJS, Tailwind CSS. Também possuo habilidades avançadas em Java e Spring para o desenvolvimento do backend.
         Com paixão pela inovação, crio experiências cativantes e humanas, buscando sempre aprender novas tecnologias para fornecer soluções criativas e eficientes. Meu objetivo principal é proporcionar uma experiência excepcional ao usuário, em qualquer dispositivo.</p>
 
