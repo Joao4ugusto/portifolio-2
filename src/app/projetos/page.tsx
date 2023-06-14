f66cfb2f1084593dffa0ff4a069a079344fe0465
João <joaoaugusto311016@gmail.com>
2023-06-14 15:32:17 -0300

    14-06

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -16,9 +16,19 @@ export default function Projetos(){
   const clouseSaidebar = () => setSidebarOpened(false)
   return(
     <div>
-      <Header openSaidebarClick={openSaidebar}/>
-      <Sidebar open={sidebarOpened} onClose={clouseSaidebar}>
+       <section className="flex flex-col w-full">
+       <Header openSaidebarClick={openSaidebar}/>
+       <Sidebar open={sidebarOpened} onClose={clouseSaidebar}>
        </Sidebar>
+       </section>
+      
+       <div className=" mx-5 my-28 md:my-28 lg:my-14 md:mx-[55px] md:ml-[330px] lg:mx-[100px] lg:ml-[400px] xl:ml-[520px] xl:mx-[240px] 2xl:ml-[565px] 2xl:mx-[300px] md:absolute">
+       <h1 className=" text-white font-semibold text-2xl">Projetos</h1>
+
+       <div className=' mt-5 w-full h-48 bg-color-black-sidebar border rounded border-stone-900'>
+
+       </div>
+       </div>
     
     <Footer />
     </div>
