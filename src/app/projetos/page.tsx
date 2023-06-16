34a9e8bdcbd780b2e5153ed8a9338c893330b574
João <joaoaugusto311016@gmail.com>
2023-06-16 11:07:37 -0300

    16-06

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -2,12 +2,8 @@
 import {useState} from 'react'
 import Sidebar from "@/components/sidebar";
 import IconArrowUpRightSquareFill from '@/components/icons/arrow';
-import IconJavascript from '@/components/icons/javaScript';
-import Gradiente from '@/components/icons/gradiente';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
-import IconSquarePen from '@/components/icons/iconPen';
-import IconAtlasian from '@/components/icons/iconPapel';
 
 export default function Projetos(){
   const [sidebarOpened, setSidebarOpened] =  useState(false);
@@ -27,9 +23,9 @@ export default function Projetos(){
        <div className=" mx-5 my-28 md:my-28 lg:my-14 md:mx-[55px] md:ml-[330px] lg:mx-[100px] lg:ml-[400px] xl:ml-[520px] xl:mx-[240px] 2xl:ml-[565px] 2xl:mx-[300px] md:absolute">
        <h1 className=" text-white font-semibold text-2xl">Projetos</h1>
 
-       <div className=' mt-5 w-full sy:w-[500px] h-52 bg-color-black-sidebar border rounded border-stone-900'>
+       <div className=' mt-5 w-full sy:w-[540px] h-52 bg-color-black-sidebar border rounded border-stone-900'>
        <div className=' h-[65%]'>
-
+        
        </div>
        <div className=' h-[35%] border-t border-stone-800 bg-black'>
          <div className=' flex m-4 justify-between items-center'>
